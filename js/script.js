268ecd553c6fdeaa7a90afd13ec1a72902191130
Bram <botw44@gmail.com>
2019-01-29 21:48:41 +0100

    Added interval

--- js/script.js
+++ js/script.js
@@ -1,6 +1,7 @@
 var pressed = false;
 var lastRandomElement = [];
 var countElements = 5;
+var switchInterval;
 
 $(document).keypress(function(event){
     var keycode = (event.keyCode ? event.keyCode : event.which);
@@ -8,16 +9,26 @@ $(document).keypress(function(event){
         // pressed enter
         if (pressed == false) {
             pressed = true;
-            setTimeout(randomGenre, 500);
+            switchInterval = setInterval(switchElements, 200);
+            setTimeout(randomGenre, 1000);
         }
     }
 });
 
+var switchElements = function() {
+    if (pressed == true) {
+        $(".bg").removeClass("active");
+        $(".bg").eq(Math.floor(Math.random() * $(".bg").length)).addClass('active');
+    } else {
+        clearInterval(switchInterval);
+    }
+}
+
 var randomGenre = function() {
+    pressed = false;
     $(".bg").removeClass("active");
     randomElement = checkRandomElement($(".bg").length);
     $(".bg").eq(randomElement).addClass('active');
-    pressed = false;
 }
 
 function checkRandomElement(length) {
